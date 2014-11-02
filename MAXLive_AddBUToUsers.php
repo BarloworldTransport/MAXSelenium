1f3179744a5d15983fd29048cce4ab13157e96dc
Clinton Wright <cwright@bwtsgroup.com>
2014-11-02 22:50:16 +0200

    Added code to use the imported data for the users. To be tested.

--- MAXLive_AddBUToUsers.php
+++ MAXLive_AddBUToUsers.php
@@ -1,9 +1,26 @@
 <?php
+// Set error reporting level for this script
+error_reporting(E_ALL);
+
+// : Includes
+
+// : Get Parent of Script Parent Directory
+$_curpath = DIRNAME(__FILE__);
+$_dirsplit = preg_split('/\//', $_curpath);
+$_x = (count($_dirsplit) - 2);
+$_newpath = (string) "";
+for ($x = 1; $x <= $_x; $x++) {
+	$_newpath .= DIRECTORY_SEPARATOR . $_dirsplit[$x];
+}
+// : End
+
 include_once 'PHPUnit/Extensions/php-webdriver/PHPWebDriver/WebDriver.php';
 include_once 'PHPUnit/Extensions/php-webdriver/PHPWebDriver/WebDriverWait.php';
 include_once 'PHPUnit/Extensions/php-webdriver/PHPWebDriver/WebDriverBy.php';
 include_once 'PHPUnit/Extensions/php-webdriver/PHPWebDriver/WebDriverProxy.php';
-include_once 'Archive/FileParser.php';
+include_once $_newpath . DIRECTORY_SEPERATOR . 'MAXreporting' . DIRECTORY_SEPERATOR . 'get_users_bu_from_list.php';
+
+// : End
 
 /**
  * web_driver_template_for_max_tests.php
@@ -17,12 +34,12 @@ include_once 'Archive/FileParser.php';
  *       it under the terms of the GNU General Public License as published by
  *       the Free Software Foundation, either version 3 of the License, or
  *       (at your option) any later version.
- *      
+ *
  *       This program is distributed in the hope that it will be useful,
  *       but WITHOUT ANY WARRANTY; without even the implied warranty of
  *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  *       GNU General Public License for more details.
- *      
+ *
  *       You should have received a copy of the GNU General Public License
  *       along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -34,7 +51,7 @@ class web_driver_template_for_max_tests extends PHPUnit_Framework_TestCase {
 	const INI_FILE = "user_data.ini";
 	const PB_URL = "/Planningboard";
 	const USER_PERSONAL_GROUP = "/DataBrowser?browsePrimaryObject=324&browsePrimaryInstance=";
-	
+
 	// : Variables
 	protected static $driver;
 	protected $_maxurl;
@@ -45,16 +62,16 @@ class web_driver_template_for_max_tests extends PHPUnit_Framework_TestCase {
 	protected $_wdport;
 	protected $_proxyip;
 	protected $_browser;
-	
+
 	// : Public Functions
 	// : Accessors
 	// : End
-	
+
 	// : Magic
 	/**
-	 * web_driver_template_for_max_tests::__construct()
-	 * Class constructor
-	 */
+	* web_driver_template_for_max_tests::__construct()
+	* Class constructor
+	*/
 	public function __construct() {
 		$ini = dirname ( realpath ( __FILE__ ) ) . self::DS . "/ini" . self::DS . self::INI_FILE;
 
@@ -83,7 +100,7 @@ class web_driver_template_for_max_tests extends PHPUnit_Framework_TestCase {
 			return FALSE;
 		}
 	}
-	
+
 	/**
 	 * web_driver_template_for_max_tests::__destruct()
 	 * Class destructor
@@ -107,143 +124,140 @@ class web_driver_template_for_max_tests extends PHPUnit_Framework_TestCase {
 			$this->_session = self::$driver->session ( $this->_browser, $desired_capabilities );
 		}
 	}
-	
+
 	/**
 	 * web_driver_template_for_max_tests::testFunctionTemplate
 	 * This is a function description for a selenium test function
 	 */
 	public function testFunctionTemplate() {
-		// Initialize session
-		$session = $this->_session;
-		$this->_session->setPageLoadTimeout ( 60 );
-		$w = new PHPWebDriver_WebDriverWait ( $session );
-		
-		try {
-			/* Before running any automation we need a list of users and their corresponding ID's to access their
-			 * data from the database, so that we can loop an array containing all the users and related data for
-			 * each user and process each and update according to a CSV file. Either this list needs to be compiled
-			 * or this needs to be determined within the script or a seperate class needs to be written to get
-			 * all users that do not have BU groups linked to them and then have the imported file amended to
-			 * define which groups are to be added to each user. The automation side is going to be quick and easy.
-			 * Quick and easy will be get user ID, load URL using ID and add groups to user. The list of users and
-			 * the BU groups to link to each is the real job here.
-			 */
-			
-			// Load MAX home page
-			$this->_session->open ( $this->_maxurl );
-			
-			// : Wait for page to load and for elements to be present on page
+		// Create new object for included class to import data
+		$_users_bu_script = new get_users_bu_from_list($_file1, $_file2);
+		// Get the user data list
+		$_data = $_users_bu_script->getData();
+		// Get the predefined list of Buiness Units
+		$_bu = $_users_bu_script->getBUList();
+
+
+		if ($_data) {
+			// Initialize session
+			$session = $this->_session;
+			$this->_session->setPageLoadTimeout ( 60 );
+			$w = new PHPWebDriver_WebDriverWait ( $session );
+
+			try {
+					
+				// Load MAX home page
+				$this->_session->open ( $this->_maxurl );
+					
+				// : Wait for page to load and for elements to be present on page
+				$e = $w->until ( function ($session) {
+					return $session->element ( 'css selector', "#contentFrame" );
+				} );
+
+					$iframe = $this->_session->element ( 'css selector', '#contentFrame' );
+					$this->_session->switch_to_frame ( $iframe );
+
+					$e = $w->until ( function ($session) {
+						return $session->element ( 'css selector', 'input[id=identification]' );
+					} );
+					// : End
+							
+						// : Assert element present
+						$this->assertElementPresent ( 'css selector', 'input[id=identification]' );
+						$this->assertElementPresent ( 'css selector', 'input[id=password]' );
+						$this->assertElementPresent ( 'css selector', 'input[name=submit][type=submit]' );
+						// : End
+							
+						// Send keys to input text box
+						$e = $this->_session->element ( 'css selector', 'input[id=identification]' )->sendKeys ( $this->_username );
+						// Send keys to input text box
+						$e = $this->_session->element ( 'css selector', 'input[id=password]' )->sendKeys ( $this->_password );
+							
+						// Click login button
+						$this->_session->element ( 'css selector', 'input[name=submit][type=submit]' )->click();
+						// Switch out of frame
+						$this->_session->switch_to_frame ();
+							
+						// : Wait for page to load and for elements to be present on page
+						$e = $w->until ( function ($session) {
+							return $session->element ( 'css selector', "#contentFrame" );
+						} );
+						$iframe = $this->_session->element ( 'css selector', '#contentFrame' );
+						$this->_session->switch_to_frame ( $iframe );
+						$e = $w->until ( function ($session) {
+							return $session->element ( "xpath", "//*[text()='" . $this->_welcome . "']" );
+						} );
+						$this->assertElementPresent ( "xpath", "//*[text()='" . $this->_welcome . "']" );
+						// Switch out of frame
+						$this->_session->switch_to_frame ();
+			} catch ( Exception $e ) {
+				throw new Exception ( "Error: Failed to log into MAX." . PHP_EOL . $e->getMessage () );
+			}
+
+			// : Load Planningboard to rid of iframe loading on every page from here on
+			$this->_session->open ( $this->_maxurl . self::PB_URL );
 			$e = $w->until ( function ($session) {
-				return $session->element ( 'css selector', "#contentFrame" );
+				return $session->element ( "xpath", "//*[contains(text(),'You Are Here') and contains(text(), 'Planningboard')]" );
 			} );
-			
-			$iframe = $this->_session->element ( 'css selector', '#contentFrame' );
-			$this->_session->switch_to_frame ( $iframe );
-			
+			// : End
+
+			// : Main Loop
+			foreach($_data as $key => $value) {
+				$this->_session->open($this->_maxurl . self::USER_PERSONAL_GROUP . $value["personalgroupid"]);
+				$e = $w->until ( function ($session) {
+					return $session->element ( "xpath", "//div[contains(text(),'{$value["firstnames"]} {$value["surname"]}')]" );
+				} );
+
+					foreach($value["bugroups"] as $bugroup) {
+							
+						$this->assertElementPresent('css selector', 'div#button-create');
+						$this->_session->element('css selector', 'div#button-create')->click();
+
+
+
+						$e = $w->until ( function ($session) {
+							return $session->element ("xpath", "//*[contains(text(),'Add a member to a group')]");
+						} );
+
+							$this->assertElementPresent("xpath", "//*[@name='Group_Role_Link[0][played_by_group_id]']");
+							$this->assertElementPresent("xpath", "//*[@name='Group_Role_Link[0][group_id]']");
+							$this->assertElementPresent("xpath", "//*[@name='Group_Role_Link[0][role_id]']");
+							$this->assertElementPresent("css selector", "input[name=save][type=submit]");
+
+							$this->_session->element("xpath", "//*[@name='Group_Role_Link[0][group_id]']/option[contains(text(), '{$_bu[$_bugroup]}')]")->click();
+							$this->_session->element("css selector", "input[name=save][type=submit]")->click();
+
+							$e = $w->until ( function ($session) {
+								return $session->element ("xpath", "//div[contains(text(),'{$value["firstnames"]} {$value["surname"]}')]");
+							} );
+
+								$this->assertElementPresent("xpath", "//a/nobr[contains(text(), '{$_bu[$_bugroup]}')]");
+								// : End
+					}
+			}
+			// : Tear Down
+			// Click the logout link
+			$this->_session->element ( 'xpath', "//*[contains(@href,'/logout')]" )->click ();
+			// Wait for page to load and for elements to be present on page
 			$e = $w->until ( function ($session) {
 				return $session->element ( 'css selector', 'input[id=identification]' );
 			} );
-			// : End
-			
-			// : Assert element present
 			$this->assertElementPresent ( 'css selector', 'input[id=identification]' );
-			$this->assertElementPresent ( 'css selector', 'input[id=password]' );
-			$this->assertElementPresent ( 'css selector', 'input[name=submit][type=submit]' );
+			// Terminate session
+			$this->_session->close ();
 			// : End
-			
-			// Send keys to input text box
-			$e = $this->_session->element ( 'css selector', 'input[id=identification]' )->sendKeys ( $this->_username );
-			// Send keys to input text box
-			$e = $this->_session->element ( 'css selector', 'input[id=password]' )->sendKeys ( $this->_password );
-			
-			// Click login button
-			$this->_session->element ( 'css selector', 'input[name=submit][type=submit]' )->click();
-			// Switch out of frame
-			$this->_session->switch_to_frame ();
-			
-			// : Wait for page to load and for elements to be present on page
-			$e = $w->until ( function ($session) {
-				return $session->element ( 'css selector', "#contentFrame" );
-			} );
-			$iframe = $this->_session->element ( 'css selector', '#contentFrame' );
-			$this->_session->switch_to_frame ( $iframe );
-			$e = $w->until ( function ($session) {
-				return $session->element ( "xpath", "//*[text()='" . $this->_welcome . "']" );
-			} );
-			$this->assertElementPresent ( "xpath", "//*[text()='" . $this->_welcome . "']" );
-			// Switch out of frame
-			$this->_session->switch_to_frame ();
-		} catch ( Exception $e ) {
-			throw new Exception ( "Error: Failed to log into MAX." . PHP_EOL . $e->getMessage () );
+		} else {
+			throw new Exception("No data imported using get_users_bu_from_list script. Quitting...");
 		}
-		
-		// : Load Planningboard to rid of iframe loading on every page from here on
-		$this->_session->open ( $this->_maxurl . self::PB_URL );
-		$e = $w->until ( function ($session) {
-			return $session->element ( "xpath", "//*[contains(text(),'You Are Here') and contains(text(), 'Planningboard')]" );
-		} );
-		// : End
-		
-		// : Main Loop
-		
-		$this->_session->open($this->_maxurl . self::USER_PERSONAL_GROUP . "2767");
-		$e = $w->until ( function ($session) {
-			return $session->element ( "xpath", "//div[contains(text(),'Abel Marimuthu')]" );
-		} );
-		
-		$this->assertElementPresent('css selector', 'div#button-create');
-		$this->_session->element('css selector', 'div#button-create')->click();
-		
-		$_tempArray = (array) array(
-			"BU - Dedicated",
-			"BU - Freight",
-			"BU - Timber 24",
-			"BU - Manline Mega",
-			"BU - Ecosse",
-			"BU - Energy"
-		);
-		
-		$_arrayPos = rand(0, (count($_tempArray) - 1));
-		
-		$e = $w->until ( function ($session) {
-			return $session->element ("xpath", "//*[contains(text(),'Add a member to a group')]");
-		} );
-		
-		$this->assertElementPresent("xpath", "//*[@name='Group_Role_Link[0][played_by_group_id]']");
-		$this->assertElementPresent("xpath", "//*[@name='Group_Role_Link[0][group_id]']");
-		$this->assertElementPresent("xpath", "//*[@name='Group_Role_Link[0][role_id]']");
-		$this->assertElementPresent("css selector", "input[name=save][type=submit]");
-		
-		$this->_session->element("xpath", "//*[@name='Group_Role_Link[0][group_id]']/option[contains(text(), '{$_tempArray[$_arrayPos]}')]")->click();
-		$this->_session->element("css selector", "input[name=save][type=submit]")->click();
-
-		$e = $w->until ( function ($session) {
-			return $session->element ("xpath", "//div[contains(text(),'Abel Marimuthu')]");
-		} );
-		
-		$this->assertElementPresent("xpath", "//a/nobr[contains(text(), '{$_tempArray[$_arrayPos]}')]");
-		// : End
-		
-		// : Tear Down
-		// Click the logout link
-		$this->_session->element ( 'xpath', "//*[contains(@href,'/logout')]" )->click ();
-		// Wait for page to load and for elements to be present on page
-		$e = $w->until ( function ($session) {
-			return $session->element ( 'css selector', 'input[id=identification]' );
-		} );
-		$this->assertElementPresent ( 'css selector', 'input[id=identification]' );
-		// Terminate session
-		$this->_session->close ();
-		// : End
 	}
-	
+
 	// : Private Functions
-	
+
 	/**
 	 * web_driver_template_for_max_tests::takeScreenshot($_session)
 	 * This is a function description for a selenium test function
 	 *
-	 * @param object: $_session        	
+	 * @param object: $_session
 	 */
 	private function takeScreenshot($_session) {
 		$_img = $_session->screenshot ();
@@ -256,18 +270,18 @@ class web_driver_template_for_max_tests extends PHPUnit_Framework_TestCase {
 			return FALSE;
 		}
 	}
-	
+
 	/**
 	 * web_driver_template_for_max_tests::assertElementPresent($_using, $_value)
 	 * This is a function description for a selenium test function
 	 *
-	 * @param string: $_using        	
-	 * @param string: $_value        	
+	 * @param string: $_using
+	 * @param string: $_value
 	 */
 	private function assertElementPresent($_using, $_value) {
 		$e = $this->_session->element ( $_using, $_value );
 		$this->assertEquals ( count ( $e ), 1 );
 	}
-	
+
 	// : End
 }
