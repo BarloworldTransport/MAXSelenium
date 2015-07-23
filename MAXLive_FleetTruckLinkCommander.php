635448ece96cd54b9303e5705fa8db538b5cadb2
Clinton Wright <cwright@hilt-cwright.bwt>
2015-07-23 16:10:57 +0200

    Commiting license change to script to GPL v2.0

--- MAXLive_FleetTruckLinkCommander.php
+++ MAXLive_FleetTruckLinkCommander.php
@@ -19,20 +19,17 @@ require_once 'FileParser.php';
  * @package MAXLive_FleetTruckLinkCommander
  * @author Clinton Wright <cwright@bwtrans.com>
  * @copyright 2013 onwards Barloworld Transport (Pty) Ltd
- * @license GNU GPL
- * @link http://www.gnu.org/licenses/gpl.html
- *       * This program is free software: you can redistribute it and/or modify
- *       it under the terms of the GNU General Public License as published by
- *       the Free Software Foundation, either version 3 of the License, or
- *       (at your option) any later version.
- *      
- *       This program is distributed in the hope that it will be useful,
- *       but WITHOUT ANY WARRANTY; without even the implied warranty of
- *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *       GNU General Public License for more details.
- *      
- *       You should have received a copy of the GNU General Public License
- *       along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * @license GNU GPLv2
+ * @link https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 class MAXLive_FleetTruckLinkCommander extends PHPUnit_Framework_TestCase
 {
